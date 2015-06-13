bf09271507fef7096e3eb56efc41abec12d8a391
Hans Idink <hidink@orgis.nl>
2015-06-13 14:15:29 +0200

    auto create history

--- dataquality.php
+++ dataquality.php
@@ -136,3 +136,99 @@ function dataquality_civicrm_preProcess($formName, &$form) {
 }
 
 */
+
+function dataquality_civicrm_custom( $op, $groupID, $entityID, &$params ) {
+    if ( $op != 'create' && $op != 'edit' ) {
+        return;
+    }
+
+   try {
+     $result = civicrm_api3('CustomGroup', 'getsingle', array(
+       'return' => "name",
+       'id' => $groupID,
+     ));
+   } catch (Exception $e){
+     CRM_Core_Error::debug_log_message("MI CUSTOM error:".$e->getMessage().$op." ".$groupID." ".$entityID." ".print_r($params,true));
+     return;
+   }
+
+   if ($result["name"] == "Pu_fields"){
+     $pu_value=-1;
+     $pu_desc="";
+     foreach ($params as $field){
+        if ($field["column_name"] == "pu_overview_1") {
+           $pu_value = $field["value"];
+        }
+        if ($field["column_name"] == "pu_contact_2") {
+           $pu_desc = $field["value"];
+        }
+
+     }
+     if ($pu_value >= 0){
+        $cid = $entityID;
+        $subject = "";
+
+   //get pu value name
+        try {
+          $resultov = civicrm_api3('OptionValue', 'getsingle', array(
+            'sequential' => 1,
+            'option_group_id' => "pu_overview_20150601084519",
+            'value' => $pu_value,
+          ));
+        } catch (Exception $e){}
+
+        if ($resultov["label"]){
+            $subject .= $resultov["label"];          
+        }
+
+        $pu_value_field = Null;
+        $pu_description_field = Null;
+        $pu_value_old = -1;
+        $pu_description_old = "";
+
+        $result = civicrm_api3('CustomField', 'get', array(
+          'name' => "new_pu_value",
+        ));
+        if ($result["id"]){ $pu_value_field = "custom_".$result["id"]; }
+        $result = civicrm_api3('CustomField', 'get', array(
+          'name' => "new_pu_description",
+        ));
+        if ($result["id"]){ $pu_description_field = "custom_".$result["id"]; }
+
+        if (isset($pu_value_field) && isset($pu_description_field)){
+          
+          $result = civicrm_api3('Activity', 'get', array(
+            'activity_type_id' => "puChanges",
+            'api.ActivityContact.get' => array(
+               'record_type_id' => "Activity Targets",
+               'contact_id' => $cid,
+            ),
+            'options' => array('sort' => "id DESC", 'limit' => 1),
+            'return' => $pu_value_field.",".$pu_description_field,
+          ));
+//          CRM_Core_Error::debug_log_message("MI CUSTOM curr activities:".print_r($result,true));
+        
+          if ($result["count"] ==1){
+              foreach ($result["values"] as $previous){ //1 expected
+                   $pu_value_old = $previous[$pu_value_field];
+                   $pu_description_old = $previous[$pu_description_field];
+              }
+          }
+
+  //        CRM_Core_Error::debug_log_message("MI CUSTOM v old:".$pu_value_old." v new:".$pu_value." d old: ". $pu_description_old. " d new:".$pu_desc);
+         
+          if ($pu_value != $pu_value_old || $pu_desc != $pu_description_old) {
+            $result = civicrm_api3('Activity', 'create', array(
+              'activity_type_id' => "puChanges",
+              $pu_value_field => $pu_value,
+              $pu_description_field => $pu_desc,
+              'target_id' => $cid,
+              'subject' => $subject,
+            ));        
+         }
+        }
+     }
+  
+   }
+   
+}
