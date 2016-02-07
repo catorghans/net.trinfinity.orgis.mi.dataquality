47fd7e63d250c30b1cf2e50c944de833c37984dd
Hans Idink <hidink@orgis.nl>
2016-02-07 15:09:12 +0100

    json config 2

--- dataquality.php
+++ dataquality.php
@@ -90,14 +90,6 @@ function dataquality_civicrm_uninstall() {
  */
 function dataquality_civicrm_enable() {
 
-    // instantiate config to create config items if required
-  /*  $settings = civicrm_api3('Setting', 'Getsingle', array());
-    require_once $settings['extensionsDir'].'/net.trinfinity.orgis.mi.dataquality/CRM/Dataquality/Config.php';
-    require_once $settings['extensionsDir'].'/net.trinfinity.orgis.mi.dataquality/CRM/Dataquality/Utils.php';
-    require_once $settings['extensionsDir'].'/net.trinfinity.orgis.mi.dataquality/CRM/Dataquality/ProfileField.php';
-    require_once $settings['extensionsDir'].'/net.trinfinity.orgis.mi.dataquality/CRM/Dataquality/ProfileGroup.php';
-    CRM_Dataquality_Config::singleton();*/
-
   _dataquality_civix_civicrm_enable();
 
     $result = civicrm_api3('Job', 'getcount', array(
